be6e24f96b8c552e3f1b0b55756389663853488a
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-17 17:20:11 +0600

    8th commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Useas  from './uses'
 import Counter from './counter'
 import Batsman from './batsman'
+
+const fetchUsers =fetch("https://jsonplaceholder.typicode.com/users").then(res=>res.json())
+
+
 function App() {
   function handleClick(){
     alert("Button Clicked!")
@@ -21,6 +25,7 @@ function App() {
        <button onClick={handleClick2}>Click M2</button>
        <Counter></Counter>
        <Batsman></Batsman>
+       
       
       
     </>
---
+++ src/uses.jsx
@@ -0,0 +1,7 @@
+export default function Useas(fetchUsers){
+    return(
+        <div className="Users">
+            <h3>Users</h3>
+        </div>
+    )
+}
