07fd0aba0752ee97e76a9d8df04ff4bb70afe454
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-17 12:08:08 +0600

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ function App() {
   function handleClick(){
     alert("Button Clicked!")
   }
+  const handleClick2 = () => {
+    alert('Button 2 clicked!')
+  }
 
   return (
     <>
       
       <h3>Vite + React</h3>
       <button onClick={handleClick}>Click Me</button>
+       <button onClick={handleClick2}>Click M2</button>
       
       
     </>
